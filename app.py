16fd0d89104f74241ab5a87ea8e815f9e039ad94
Mehul Verma <mehulverma17@gmail.com>
2024-06-19 15:38:53 +0530

    updated polarity and table

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# app.py
-
 from flask import Flask, render_template, request
 from youtubeCommentScrapper import get_youtube_comments, extract_video_id
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -8,41 +6,28 @@ import re
 
 app = Flask(__name__)
 
-def sentiment_scores(comment, polarity):
- 
-    # Creating a SentimentIntensityAnalyzer object.
+def sentiment_scores(comment):
     sentiment_object = SentimentIntensityAnalyzer()
- 
     sentiment_dict = sentiment_object.polarity_scores(comment)
-    polarity.append(sentiment_dict['compound'])
- 
-    return polarity
+    return sentiment_dict['compound']
 
-def get_relevant_comments(comment):
+def get_relevant_comments(comments):
     hyperlink_pattern = re.compile(
-	r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-
+        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    )
     threshold_ratio = 0.65
-
     relevant_comments = []
 
-    # Inside your loop that processes comments
-    for comment_text in comment:
-
+    for comment_text in comments:
         comment_text = comment_text.lower().strip()
-
         emojis = emoji.emoji_count(comment_text)
-
-        # Count text characters (excluding spaces)
         text_characters = len(re.sub(r'\s', '', comment_text))
 
         if (any(char.isalnum() for char in comment_text)) and not hyperlink_pattern.search(comment_text):
             if emojis == 0 or (text_characters / (text_characters + emojis)) > threshold_ratio:
                 relevant_comments.append(comment_text)
 
-    # Print the relevant comments
     return relevant_comments
-
 
 @app.route('/')
 def index():
@@ -53,7 +38,7 @@ def fetch_comments():
     url = request.form['link']
     try:
         video_id = extract_video_id(url)
-        comments,list_comments = get_youtube_comments(video_id)
+        comments, list_comments = get_youtube_comments(video_id)
         response = None
         polarity = []
         positive_comments = []
@@ -61,50 +46,47 @@ def fetch_comments():
         neutral_comments = []
 
         relevant_comments = get_relevant_comments(list_comments)
-        
-        for items in relevant_comments:
-            polarity = sentiment_scores(items, polarity)
-        
-            if polarity[-1] > 0.05:
-                positive_comments.append(items)
 
-            elif polarity[-1] < -0.05:
-                negative_comments.append(items)
+        for comment in relevant_comments:
+            score = sentiment_scores(comment)
+            polarity.append(score)
 
+            if score > 0.1:  # Slightly increase the threshold for positive
+                positive_comments.append(comment)
+            elif score < -0.1:  # Slightly increase the threshold for negative
+                negative_comments.append(comment)
             else:
-                neutral_comments.append(items)
-        
-        avg_polarity = sum(polarity)/len(polarity)
+                neutral_comments.append(comment)
+
+        avg_polarity = sum(polarity) / len(polarity) if polarity else 0
 
         positive_count = len(positive_comments)
         negative_count = len(negative_comments)
-        neutral_count = len(relevant_comments)-positive_count-negative_count
+        neutral_count = len(neutral_comments)
 
-        print("average Polarity: ",avg_polarity)
+        print("average Polarity: ", avg_polarity)
         if avg_polarity > 0.05:
             response = "The Video has got a Positive response"
         elif avg_polarity < -0.05:
             response = "The Video has got a Negative response"
         else:
             response = "The Video has got a Neutral response"
-        
-        print("The comment with most positive sentiment:", comments[polarity.index(max(
-            polarity))], "with score", max(polarity), "and length", len(comments[polarity.index(max(polarity))]))
-        print("The comment with most negative sentiment:", comments[polarity.index(min(
-            polarity))], "with score", min(polarity), "and length", len(comments[polarity.index(min(polarity))]))
+
+        if polarity:
+            print("The comment with most positive sentiment:", comments[polarity.index(max(
+                polarity))], "with score", max(polarity), "and length", len(comments[polarity.index(max(polarity))]))
+            print("The comment with most negative sentiment:", comments[polarity.index(min(
+                polarity))], "with score", min(polarity), "and length", len(comments[polarity.index(min(polarity))]))
 
         limited_comments = comments[:10]
 
-
-        return render_template('index.html', comments=comments, limited_comments=limited_comments, response=response, positive_count=positive_count, negative_count=negative_count, neutral_len=neutral_count)
+        return render_template('index.html', comments=comments, limited_comments=limited_comments, response=response, positive_count=positive_count, negative_count=negative_count, neutral_count=neutral_count)
     
     except ValueError as e:
         return render_template('index.html', error=str(e))
     
     except Exception as e:
         return render_template('index.html', error="An error occurred while fetching comments.")
-    
-
 
 if __name__ == '__main__':
     app.run(debug=True)
